3a34dba861b7f8e0e8b0cfd6859d73c81cc7e460
otabekablayev1-ai <otabek.ablayev1@gmail.com>
2026-02-06 15:27:42 +0500

    Fix DefaultBotProperties import (aiogram 3)

--- loader.py
+++ loader.py
@@ -1,6 +1,7 @@
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 from data import config
 
