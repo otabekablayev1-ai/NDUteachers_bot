07b5b27dec87ba20d0964773b4048e4aab6bcd64
otabekablayev1-ai <otabek.ablayev1@gmail.com>
2026-05-12 11:42:50 +0500

    Update db.py

--- database/db.py
+++ database/db.py
@@ -351,6 +351,7 @@ async def get_manager_rating_table() -> list[dict]:
     EXCLUDED_MANAGER_IDS = {
         1040796931,
         8722281391,
+        810299183,
         1017100005, # 🔥 siz aytgan admin ID
     }
 
